7219868fa9bf75e2005ea4eb0f0c1b7f48f1c185
pjc116 <a0979003552@gmail.com>
2025-12-10 11:55:20 +0800

    week14

---
+++ week14/week14-2.cpp
@@ -0,0 +1,22 @@
+//week14-2.cpp 學習計畫 Linked List 第2題
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr || head->next==nullptr) return head;
+        ListNode* ans = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return ans;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
---
+++ week14/week14-3.cpp
@@ -0,0 +1,30 @@
+//week14-3.cpp 學習計畫 Linked List 第2題 換另一種寫法
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        vector<int> a;
+        while(head != nullptr){
+            a.push_back(head->val);
+            head = head->next;
+        }
+
+        ListNode* ans = new ListNode(999);
+        ListNode* now = ans;
+        for(int i=a.size()-1; i>=0; i--){
+            now->next = new ListNode(a[i]);
+            now = now->next;
+        }
+        return ans->next;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
